21cbec041c7ec2325d55c830fff18256859b74ef
harpo <harpo>
2017-08-24 10:08:47 -0300

    Libreria para AlGO I UNcuyo

---
+++ algo1.py
@@ -0,0 +1,53 @@
+# Paquete algo1.py v3
+# Algoritmos y Estructuaras de datos I
+# Funciones de carga de valores
+import copy
+def input_int( str ):
+	try:
+		ingreso=int(float(input( str )))
+	except:
+		ingreso=0
+	return ingreso
+
+def input_real( str ):
+	try:
+		ingreso=float(input( str ))
+	except:
+		ingreso=0.0
+	return ingreso
+
+def input_str( str ):
+	try:
+		ingreso=input( str )
+	except:
+		ingreso=""
+	return ingreso
+
+# Clase arreglos
+class array:
+        data=[]
+        def __init__(self,size=None,init_value=0):
+		print type(init_value)
+                if size == None:
+                        self.size=0
+                else:
+                        self.size=size
+                self.data= [copy.deepcopy(init_value) for i in range(0,size)]
+                self.type = type(init_value)
+        def __getitem__(self,index):
+                if index > self.size:
+                        print ("IndexError: index Out of bounds")
+                else:
+                        return self.data[index]
+        def __setitem__(self,index,value):
+                if index > self.size:
+                        print ("IndexError: index Out of bounds")
+                elif type(value) != self.type:
+                        print ("TypeError: value error")
+                else:
+                        self.data[index]=value
+        def __str__(self):
+                return str([self.data[i] for i in range(0,len(self.data))])
+
+        def __len__(self):
+                return(self.size)
